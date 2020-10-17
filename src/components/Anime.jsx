fcc4570e5df90a96ec7f7026173b837ab75e24a2
Leonel Gauna <leonel.mariano.g@gmail.com>
2020-10-17 19:43:57 -0300

    Add styles for anime page

--- src/components/Anime.jsx
+++ src/components/Anime.jsx
@@ -5,6 +5,83 @@ import useInstantFetch from "../hooks/instant-fetch/useInstantFetch.js";
 import jikanApi from "../helpers/api.js";
 import { fromJSONToAnimeEntity } from "../helpers/mappers";
 
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+
+  & > * {
+    display: block;
+    margin-top: 30px;
+  }
+
+  & > *:last-child {
+    margin-bottom: 30px;
+  }
+`;
+
+const Title = styled.h1`
+  @media screen and (max-width: 600px) {
+    & {
+      font-size: 20px;
+    }
+  }
+`;
+
+const Image = styled.img`
+  border-radius: 10px;
+`;
+
+const TextContent = styled.div`
+  background: #303030;
+  padding: 20px;
+  border-radius: 10px;
+  width: 80%;
+  line-height: 25px;
+  letter-spacing: 1px;
+`;
+
+const IconContainer = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  align-items: center;
+`;
+
+const Icon = styled.div`
+  background: #ff9d00;
+  padding: 10px;
+  border-radius: 10px;
+  box-shadow: -5px -5px 0px 0px #ff8400 inset,
+    5px 5px 0px 0px rgba(0, 0, 0, 0.2);
+  color: #000000;
+  margin: 20px;
+`;
+
+const IFrameWrapper = styled.div`
+  position: relative;
+  overflow: hidden;
+  width: 80%;
+  padding-top: 40%;
+`;
+
+const IFrame = styled.iframe`
+  position: absolute;
+  top: 0;
+  left: 0;
+  bottom: 0;
+  right: 0;
+  width: 100%;
+  height: 100%;
+`;
+
+const withoutAutoPlay = (url) =>
+  url
+    .split("&")
+    .filter((element) => !element.includes("autoplay"))
+    .join("&");
+
 const Anime = () => {
   const { animeId } = useParams();
   const { data, loading, error } = useInstantFetch(
@@ -14,15 +91,26 @@ const Anime = () => {
   );
 
   const page = data ? (
-    <div>
-      <h1>{data.title}</h1>
-      <img src={data.imageUrl} alt={data.title} />
-      <p>{data.synopsis}</p>
-      <p>{data.episodes}</p>
-      <p>{data.score}</p>
-      <p>{data.rating}</p>
-      <p>{data.trailerUrl}</p>
-    </div>
+    <Wrapper>
+      <Title>{data.title}</Title>
+      <Image src={data.imageUrl} alt={data.title} />
+      <TextContent>{data.synopsis}</TextContent>
+      <IconContainer>
+        <Icon>Episodes: {data.episodes}</Icon>
+        <Icon>Score: {data.score}</Icon>
+        <Icon>Rating: {data.rating}</Icon>
+      </IconContainer>
+      {data.trailerUrl ? (
+        <IFrameWrapper>
+          <IFrame
+            title={`Trailer for ${data.title}`}
+            src={withoutAutoPlay(data.trailerUrl)}
+            frameBorder="0"
+            allowFullScreen
+          />
+        </IFrameWrapper>
+      ) : null}
+    </Wrapper>
   ) : null;
 
   return (
