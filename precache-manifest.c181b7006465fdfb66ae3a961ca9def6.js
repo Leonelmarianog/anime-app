7b5150148f0e2eefec0a6b0f29bc3e359f9e007c
Leonel Gauna <leonel.mariano.g@gmail.com>
2020-10-17 21:42:56 -0300

    Updates

---
+++ precache-manifest.c181b7006465fdfb66ae3a961ca9def6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "229edd02e79d2ea9c3629433edcba192",
+    "url": "/anime-app/index.html"
+  },
+  {
+    "revision": "33250458d179b884f229",
+    "url": "/anime-app/static/js/2.913925b9.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/anime-app/static/js/2.913925b9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b9b4d6bb68061ec477b3",
+    "url": "/anime-app/static/js/main.d37119c1.chunk.js"
+  },
+  {
+    "revision": "94b6ef9deaa5329cb10f",
+    "url": "/anime-app/static/js/runtime-main.2f9bb856.js"
+  }
+]);
